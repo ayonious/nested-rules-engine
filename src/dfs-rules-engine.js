65fb1274371123dcc1730cc9d335ceee38149198
Nahiyan Kamal <noyanayihan@gmail.com>
2018-12-15 22:57:36 +0100

    adding new features

--- src/dfs-rules-engine.js
+++ src/dfs-rules-engine.js
@@ -1,16 +1,36 @@
-const serialTraverse = (inputs, functions, tree) => {
+const serialTraverse = (inputs, functions, tree, options) => {
     let current = tree;
+    let isVerbose = options && options.verbose === true;
+    let verboseOutput = [];
+
+    const createOutput = (executable, inputs, verboseOutput) => {
+        return {
+            result: executable(inputs),
+            logs: verboseOutput
+        };
+    };
+
+    const getVerbose = ( text ) => {
+        if(isVerbose) { 
+            verboseOutput.push( text );
+        }
+    }
+
     while(true) {
         if (current === null) {
             break;
         }
+            
         //base case
         if(typeof current !== 'object') {
-            return functions[current](inputs);
+            getVerbose( `Executing Function ${current}`);
+            return createOutput(functions[current], inputs, verboseOutput);
         }
         //todo check for empty object
         for(var key in current) {
+            getVerbose(`Executing Function ${key}`);
             const thisRes = functions[key](inputs);
+            getVerbose(`Result of Function ${key} is ${thisRes}`);
             if( thisRes === true ) {
                 current = current[key];
                 break;
@@ -20,16 +40,23 @@ const serialTraverse = (inputs, functions, tree) => {
 	return null;
 };
 
-const parallelTraverse =  (inputs, functions, trees) => {
+const parallelTraverse =  (inputs, functions, trees, options) => {
     res = [];
     for (let tree of trees) {
-        res.push(serialTraverse(inputs, functions, tree));
+        res.push(serialTraverse(inputs, functions, tree, options));
     }
     return res;
 };
 
-module.exports = {
-    serialTraverse: serialTraverse,
-    parallelTraverse: parallelTraverse
+
+const executeEngine =  (inputs, functions, trees, options) => {
+    if(options && options.isParallel === true) {
+        return parallelTraverse(inputs, functions, trees, options);
+    } else {
+        return serialTraverse(inputs, functions, trees, options);
+    }
 };
 
+module.exports = {
+    executeEngine
+};
--- test/run-tests.js
+++ test/run-tests.js
@@ -1,19 +1,120 @@
-const {parallelTraverse} = require('../src/dfs-rules-engine');
+const {executeEngine} = require('../src/dfs-rules-engine');
 const {expect} = require('chai');
-const rules = require('./test1/sample');
-const functions = require('./test1/functions.js');
-const inputs = require('./test1/inputs');
 
 
-describe('Simplest test should pass', () => {
-    it(`test1`, function () {
-        const res = parallelTraverse(inputs, functions, rules);
+describe('Simplest parallel test should pass', () => {
+    const rules = require('./test1/sample');
+    const functions = require('./test1/functions.js');
+    const inputs = require('./test1/inputs');
+
+    it(`parallel traverse should work`, function () {
+        const options = {
+            isParallel: true,
+            verbose: false
+        };
+
+        const res = executeEngine(inputs, functions, rules, options);
         expect(res).to.deep.equal([{
-            payload: 'doing homework', 
-            effort: 'im getting sick' 
+            result : {
+                payload: 'doing homework', 
+                effort: 'im getting sick' 
+            },
+            logs: []
         }, { 
-            getting: 'my car',
-            finding: 'my phone' 
+            result : {
+                getting: 'my car',
+                finding: 'my phone' 
+            },
+            logs: []
         }]);
+    });
+    it(`parallel traverse should work with verbose output`, function () {
+        const options = {
+            isParallel: true,
+            verbose: true
+        };
+        const res = executeEngine(inputs, functions, rules, options);
+
+        expect(res).to.deep.equal([{
+            result : {
+                payload: 'doing homework', 
+                effort: 'im getting sick' 
+            }, logs: [
+                "Executing Function you_are_a_human",
+                "Result of Function you_are_a_human is true",
+                "Executing Function you_are_not_racist",
+                "Result of Function you_are_not_racist is true",
+                "Executing Function you_are_kind",
+                "Result of Function you_are_kind is false",
+                "Executing Function you_are_smart",
+                "Result of Function you_are_smart is true",
+                "Executing Function you_live_near_my_house",
+                "Result of Function you_live_near_my_house is true",
+                "Executing Function you_like_people",
+                "Result of Function you_like_people is true",
+                "Executing Function please_do_my_homework",
+          ]
+        }, { 
+            result : {
+                getting: 'my car',
+                finding: 'my phone' 
+            }, logs: [
+                "Executing Function you_feel_good",
+                "Result of Function you_feel_good is false",
+                "Executing Function you_feel_shit",
+                "Result of Function you_feel_shit is true",
+                "Executing Function you_have_temperature",
+                "Result of Function you_have_temperature is true",
+                "Executing Function goto_doctor",
+          ]
+        }]);
+    });
+});
+
+
+
+describe('Simplest serial test should pass', () => {
+    const rules = require('./test2/sample');
+    const functions = require('./test2/functions.js');
+    const inputs = require('./test2/inputs');
+
+    it(`serial traverse should work`, function () {
+        const res = executeEngine(inputs, functions, rules);
+        const serialExpectedOutput = {
+            result : {
+                payload: 'doing homework', 
+                effort: 'im getting sick' 
+            },
+            logs: []
+        };
+        expect(res).to.deep.equal(serialExpectedOutput);
+    });
+
+    it(`serial traverse should work with verbose output`, function () {
+        const options = {
+            verbose: true
+        };
+        const res = executeEngine(inputs, functions, rules, options);
+        const expectedSerialVerboseRes = {
+            result : {
+                payload: 'doing homework', 
+                effort: 'im getting sick' 
+            }, logs: [
+                "Executing Function you_are_a_human",
+                "Result of Function you_are_a_human is true",
+                "Executing Function you_are_not_racist",
+                "Result of Function you_are_not_racist is true",
+                "Executing Function you_are_kind",
+                "Result of Function you_are_kind is false",
+                "Executing Function you_are_smart",
+                "Result of Function you_are_smart is true",
+                "Executing Function you_live_near_my_house",
+                "Result of Function you_live_near_my_house is true",
+                "Executing Function you_like_people",
+                "Result of Function you_like_people is true",
+                "Executing Function please_do_my_homework",
+          ]
+        };
+        expect(res).to.deep.equal(expectedSerialVerboseRes);
     });
 });
